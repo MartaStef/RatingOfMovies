5ebed9cf5d39c61f34cfecab8db3c8f91a94d77a
Marta Stefanska <marta.stefanska@outlook.es>
2024-03-10 00:10:43 +0100

    Pierwsza wersja mojej pierwszej apki

---
+++ RatingOfMovies/RatingOfMovies/Movie.cs
@@ -0,0 +1,30 @@
+﻿namespace RatingOfMovies
+{
+    public class Movie
+    {
+        private List<int> ratings = new List<int>();
+        public Movie (string title, string releaseYear, string description)
+        {
+            this.Title = title;
+            this.ReleaseYear = releaseYear;
+            this.Description = description;
+        }
+
+        public string Title { get; private set; }
+        public string ReleaseYear { get; private set;}
+        public string Description { get; private set; }
+        
+        public int Result
+        {
+            get
+            {
+                return this.ratings.Sum();
+            }
+        }
+        public void AddRating (int rating)
+        {
+            this.ratings.Add (rating);
+        }
+
+    }
+}
--- RatingOfMovies/RatingOfMovies/Program.cs
+++ RatingOfMovies/RatingOfMovies/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RatingOfMovies;
+
+Movie movie1 = new Movie("Interstellar", "2014", "space");
+Movie movie2 = new Movie("I,Robot", "2004", "robot");
+Movie movie3 = new Movie("WALL.E", "2008", "robot");
+Movie movie4 = new Movie("Matrix", "1999", "simulation");
+
+List<Movie> movies = new List<Movie>()
+{ movie1, movie2, movie3, movie4 };
+
+foreach (var movie in movies)
+{
+    while (true)
+    {
+        Console.WriteLine("Podaj ocenę filmu:" + movie.Title);
+        var input = Console.ReadLine();
+        if (input == "next")
+        { break; }
+        var rating = int.Parse(input);
+        movie.AddRating(rating);
+    }
+}
+
+int maxResult = 0;
+
+Movie bestMovie = null;
+
+foreach (var movie in movies)
+{
+    if (movie.Result > maxResult)
+    {
+        bestMovie = movie;
+        maxResult = movie.Result;
+    }
+}
+
+Console.WriteLine($"Najlepszy film pod tytułem: " + bestMovie.Title + " z " + bestMovie.ReleaseYear +
+    " roku otrzymał" + bestMovie.Result + " punktów");
